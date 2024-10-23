7b7997a327ecc2a417f1c0a38f82de502b786bca
NightShiftKing <98564476+NightShiftKing@users.noreply.github.com>
2024-10-23 15:18:18 -0600

    Add files via upload

--- Python_DailyCodes/functions/Distance.py
+++ Python_DailyCodes/functions/Distance.py
@@ -4,10 +4,11 @@ import math
 x1 = int(input("what is the value of x1?"))
 x2 = int(input("what is the value of x2?"))
 y1 = int(input("what is the value of y1?"))
+
 y2 = int(input("what is the value of y2?"))
 
 def Distance(x1,x2,y1,y2):
     distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2) 
     print(distance)
 
-Distance(x1, x2, y1, y2)
+Distance(x1,x2,y1,y2)
