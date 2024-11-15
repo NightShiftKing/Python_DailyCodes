925664e3c1daf7cf9257ff79dd57dad20e3e9195
NightShiftKing <98564476+NightShiftKing@users.noreply.github.com>
2024-11-15 13:53:07 -0700

    Add files via upload

---
+++ Python_DailyCodes/List/ListQuiz.py
@@ -0,0 +1,15 @@
+zoo = ['a Monkey ', 'an Elephant ', 'a Lion ', 'a Tiger ', 'a Giraffe ']
+
+print(zoo)
+
+zoo.append('a panda ')
+print(zoo)
+
+print(len(zoo))
+
+zoo.remove('a Lion ')
+print(zoo)
+
+zoo.insert(1, 'a Meerkat ')
+print(zoo)
+
